0b52666c1f5c8ec9afd87e42b043af1f0b0393f2
dofoyo <dofoyo@sina.cn>
2014-07-31 16:38:44 +0800

    将uuid放到services中

--- app/process/js/processControllers.js
+++ app/process/js/processControllers.js
@@ -8,10 +8,8 @@ pc.controller('ProcessCtrl',[
     '$window',
     '$fileUploader',
     'CommentType',
-    function($scope, $http, $templateCache, $window,$fileUploader,CommentType){
-
-    alert(CommentType.EXECUTER);
-
+    'uuid',
+    function($scope, $http, $templateCache, $window,$fileUploader,CommentType,uuid){
     $scope.processes = new Array();
 
     $scope.process = {
@@ -257,12 +255,12 @@ pc.controller('ProcessCtrl',[
         if($window.sessionStorage.token){
             var subject = $scope.process.subject;
             var description = $scope.process.description;
-
+            var id = uuid;
             subject = subject.replace(/[\n\r]/g,'').replace(/[\\]/g,'');
             description = description.replace(/[\n\r]/g,'').replace(/[\\]/g,'');
 
             var process = {
-                id:uuid(24,11),
+                id:id,
                 subject:subject,
                 description:description,
                 createDatetime:new Date(),
@@ -549,31 +547,3 @@ function saveProcess($http,$templateCache,jdata ){
         });
 }
 
-function uuid(len, radix) {
-    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-    var uuid = [], i;
-    radix = radix || chars.length;
-
-    if (len) {
-        // Compact form
-        for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
-    } else {
-        // rfc4122, version 4 form
-        var r;
-
-        // rfc4122 requires these characters
-        uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
-        uuid[14] = '4';
-
-        // Fill in random data.  At i==19 set the high bits of clock sequence as
-        // per rfc4122, sec. 4.1.5
-        for (i = 0; i < 36; i++) {
-            if (!uuid[i]) {
-                r = 0 | Math.random()*16;
-                uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
-            }
-        }
-    }
-
-    return uuid.join('');
-}
